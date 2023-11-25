2673c39156aea6884c98fde5e33055b359e00038
이민성 <133635509+minsung-phy@users.noreply.github.com>
2023-11-25 22:03:57 +0900

    Add files via upload

--- 11주차/neuralNetwork.py
+++ 11주차/neuralNetwork.py
@@ -1,50 +1,61 @@
 import numpy as np
 import math
 
+# neuralNetwork class definition
 class neuralNetwork:
-  bih = 0.35      # ]
-  bho = 0.60      # ]바이어스들
-  eta = 0.5       # 학습률
+    # weights of bias
+    bih = 0.35 
+    bho = 0.60
+    # learning rate
+    eta = 0.5 
 
-  def __init__(self, wih, who):
-    self.wih = wih
-    self.who = who
-    self.sig = lambda x: 1/(1+pow(math.e,-x))
-    self.sigp = lambda x: pow(math.e,-x)/(1+pow(math.e,-x))**2
+    # initialise the neuralNetwork
+    def __init__(self, wih, who): 
+        # link weights
+        self.wih = wih
+        self.who = who
+        # sigmoid function
+        self.sig = lambda x: 1 / (1 + pow(math.e, -x))
+        self.sigp = lambda x : pow(math.e, -x) / (1 + pow(math.e, -x)) ** 2   
 
-    #self 함수로 class의 모든 장소에서 함수와 변수를 사용가능, pow 승을 의미하는 함수
+        pass     
 
-    pass
+    # train the neuralNetwork
+    def train(self, inputs_list, targets_list):
+        # convert inputs list to 2d array
+        inputs = np.array(inputs_list, ndmin = 2).T
+        targets = np.array(targets_list, ndmin = 2).T
+        # calculate signals into hidden layer
+        hidden_inputs = np.dot(self.wih, inputs) + self.bih * np.ones((2,1))
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.sig(hidden_inputs)
+        # calculate signals into final output layer
+        final_inputs = np.dot(self.who, hidden_outputs) + self.bho * np.ones((2, 1))
+        # calculate the signals emerging from output layer
+        final_outputs = self.sig(final_inputs)
 
-  def train(self, inputs_list, targets_list):
-    inputs = np.array(inputs_list,ndmin=2).T
-    targets = np.array(targets_list,ndmin=2).T
-    hidden_inputs = np.dot(self.wih, inputs)+self.bih*np.ones((2,1))
-    hidden_outputs = self.sig(hidden_inputs)
-    final_inputs = np.dot(self.who, hidden_outputs)+self.bho*np.ones((2,1))
-    final_outputs = self.sig(final_inputs)
+        # partial derivative of E with respect to weight
+        output_errors = final_outputs - targets
+        a = np.dot((output_errors * self.sigp(final_inputs)).T, self.who).T
+        dEdw_ih = a * self.sigp(hidden_inputs) * inputs.T
+        dEdw_ho = output_errors * self.sigp(final_inputs) * hidden_outputs.T
 
-    #인풋 리스트를 행렬로 만들어준 뒤, 전치하여 계산하는 단계. dot이 행렬계산. ones는 1로 이루어진 행렬 구성. 이로 만들어진 히든 인풋을 self.sig에 넣어 히든 아웃풋 취득. 비슷한 과정을 반복
+        # update the weights for the links between the hidden and output layers
+        self.wih -= self.eta * dEdw_ih
+        # update the weights for the links between the input and hidden layers
+        self.who -= self.eta * dEdw_ho
 
-    output_errors = final_outputs - targets
-    a = np.dot((output_errors*self.sigp(final_inputs)).T,self.who).T
-    dEdw_ih = a*self.sigp(hidden_inputs)*inputs.T
-    dEdw_ho = output_errors*self.sigp(final_inputs)*hidden_outputs.T
+        pass
 
-    self.wih -= self.eta*dEdw_ih
-    self.who -= self.eta*dEdw_ho
+    def query(self,inputs_list):
+        inputs = np.array(inputs_list, ndmin=2).T
+        hidden_inputs = np.dot(self.wih, inputs)+self.bih*np.ones((2,1))
+        hidden_outputs = self.sig(hidden_inputs)
+        final_inputs = np.dot(self.who, hidden_outputs)+self.bho*np.ones((2,1))
+        final_outputs = self.sig(final_inputs)
 
-    pass
-
-  def query(self,inputs_list):
-    inputs = np.array(inputs_list, ndmin=2).T
-    hidden_inputs = np.dot(self.wih, inputs)+self.bih*np.ones((2,1))
-    hidden_outputs = self.sig(hidden_inputs)
-    final_inputs = np.dot(self.who, hidden_outputs)+self.bho*np.ones((2,1))
-    final_outputs = self.sig(final_inputs)
-
-    return final_outputs
-
+        return final_outputs
+    
 wih = np.array([[0.15,0.20],[0.25,0.30]])
 who = np.array([[0.40,0.45],[0.50,0.55]])
 n = neuralNetwork(wih, who)
